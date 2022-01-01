b47f2e2b794759ecfdc6bf775127a5c2a96daef4
korakg <a0975979717@gmail.com>
2022-01-01 16:11:05 +0800

    1

--- js/web_page3.js
+++ js/web_page3.js
@@ -32,4 +32,52 @@ $(function(){
             $("#v2").addClass("switch4")
         }
     })
+    $("#wall-1 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info1").removeClass("hide")
+    })
+    $("#wall-2 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info2").removeClass("hide")
+    })
+    $("#wall-3 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info3").removeClass("hide")
+    })
+    $("#wall-4 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info4").removeClass("hide")
+    })
+    $("#wall-5 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info5").removeClass("hide")
+    })
+    $("#wall-6 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info6").removeClass("hide")
+    })
+    $("#wall-7 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info7").removeClass("hide")
+    })
+    $("#wall-8 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info8").removeClass("hide")
+    })
+    $("#wall-9 a").click(function(){
+        $("#infobox").removeClass("hide")
+        $("#info9").removeClass("hide")
+    })
+    $("#infobox").click(function(){
+        $("#infobox").addClass("hide")
+        $("#info1").addClass("hide")
+        $("#info2").addClass("hide")
+        $("#info3").addClass("hide")
+        $("#info4").addClass("hide")
+        $("#info5").addClass("hide")
+        $("#info6").addClass("hide")
+        $("#info7").addClass("hide")
+        $("#info8").addClass("hide")
+        $("#info9").addClass("hide")
+    })
 })
