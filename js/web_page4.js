2f35384e0f308fdbdc748526afed9bcfcd8dd9e8
korakg <a0975979717@gmail.com>
2022-01-05 16:56:41 +0800

    1

--- js/web_page4.js
+++ js/web_page4.js
@@ -1,3 +1,20 @@
 $(function(){
-    
+    $(window).scroll(function() {
+        var scrollVal = $(this).scrollTop();
+        if (scrollVal >= 380) {
+            $("#section_1>div").addClass("in")
+            $("#section_1>h1").addClass("word_show")
+            $("#section_1>p").addClass("word_show")
+            $("#section_1>div").removeClass("out")
+            $("#section_1>h1").removeClass("word_hide")
+            $("#section_1>p").removeClass("word_hide")
+        } else {
+            $("#section_1>div").addClass("out")
+            $("#section_1>h1").addClass("word_hide")
+            $("#section_1>p").addClass("word_hide")
+            $("#section_1>div").removeClass("in")
+            $("#section_1>h1").removeClass("word_show")
+            $("#section_1>p").removeClass("word_show")
+        }
+    });
 })
